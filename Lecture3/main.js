d88f011cf8531d47b3a03082f5becea33b53638e
abhishekbanerjee2k24 <abhishekbanerjee2k24@gmail.com>
2025-10-17 08:18:32 +0000

    Added logical and relational Operatos

---
+++ Lecture3/main.js
@@ -0,0 +1,9 @@
+console.log(undefined && null);
+console.log(null && 10);
+console.log(10 && -0);
+console.log(10 && 20);
+console.log();
+console.log(undefined || null);
+console.log("" || 10);
+console.log(10 || -0);
+console.log(10 || 20);
---
+++ Lecture3/main1.js
@@ -0,0 +1,18 @@
+// This is a comment 
+/* 
+  ==  --> Abstract equality  : Means equals or not and if equals or not after sonversion
+  === --> Strict equality
+  !=  --> Abstract inequality
+  !== --> Strict inequality 
+
+*/
+console.log(10 == 10);
+console.log("10" == 10);
+console.log();
+console.log(10 === 10);
+console.log("10" === 10);
+console.log();
+console.log(10 != 10);
+console.log("10" != 10);
+console.log(10 !== 10);
+console.log("10" !== 10);
